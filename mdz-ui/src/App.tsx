23ac7e0d9b77d6089ecdd5b0746158ee8f43ef7d
mohamedaliznidi <57480839+mohamedaliznidi@users.noreply.github.com>
2024-02-21 15:27:21 +0000

    using custom styling

--- mdz-ui/src/App.tsx
+++ mdz-ui/src/App.tsx
@@ -6,44 +6,44 @@ function App() {
     <div className="w-screen flex  justify-items-center flex-col">
       {/* // primary */}
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'primary'} size={'medium'} appearence={"solid"} onPress={() => alert('Hello world!')}>primary</MDZButton>
-        <MDZButton intent={'primary'} size={'small'} appearence={"outline"} onPress={() => alert('Hello world!')}>primary</MDZButton>
+        <MDZButton color='primary' variant="solid" >primary</MDZButton>
+        <MDZButton color={'primary'} variant="outline" onClick={() => alert('Hello world!')}>primary</MDZButton>
       </div> <br/>
       {/* // secondary */}
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'secondary'} size={'small'} onPress={() => alert('Hello world!')}>secondary</MDZButton>
-        <MDZButton intent={'secondary'} size={'medium'} onPress={() => alert('Hello world!')}>secondary</MDZButton>
-        <MDZButton intent={'secondary'} size={'large'} onPress={() => alert('Hello world!')}>secondary</MDZButton>
+        <MDZButton color={'secondary'} size={'small'} onPress={() => alert('Hello world!')}>secondary</MDZButton>
+        <MDZButton color={'secondary'} size={'medium'} onPress={() => alert('Hello world!')}>secondary</MDZButton>
+        <MDZButton color={'secondary'} size={'large'} onPress={() => alert('Hello world!')}>secondary</MDZButton>
       </div>
       {/* // success */}<br />
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'success'} size={'small'} onPress={() => alert('Hello world!')}>success</MDZButton>
-        <MDZButton intent={'success'} size={'medium'} onPress={() => alert('Hello world!')}>success</MDZButton>
-        <MDZButton intent={'success'} size={'large'} onPress={() => alert('Hello world!')}>success</MDZButton>
+        <MDZButton color={'success'} size={'small'} onPress={() => alert('Hello world!')}>success</MDZButton>
+        <MDZButton color={'success'} size={'medium'} onPress={() => alert('Hello world!')}>success</MDZButton>
+        <MDZButton color={'success'} size={'large'} onPress={() => alert('Hello world!')}>success</MDZButton>
       </div>
       {/* // error */}<br />
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'error'} size={'small'} onPress={() => alert('Hello world!')}>error</MDZButton>
-        <MDZButton intent={'error'} size={'medium'} onPress={() => alert('Hello world!')}>error</MDZButton>
-        <MDZButton intent={'error'} size={'large'} onPress={() => alert('Hello world!')}>error</MDZButton>
+        <MDZButton color={'error'} size={'small'} onPress={() => alert('Hello world!')}>error</MDZButton>
+        <MDZButton color={'error'} size={'medium'} onPress={() => alert('Hello world!')}>error</MDZButton>
+        <MDZButton color={'error'} size={'large'} onPress={() => alert('Hello world!')}>error</MDZButton>
       </div>
       {/* // warning */}<br />
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'warning'} size={'small'} onPress={() => alert('Hello world!')}>warning</MDZButton>
-        <MDZButton intent={'warning'} size={'medium'} onPress={() => alert('Hello world!')}>warning</MDZButton>
-        <MDZButton intent={'warning'} size={'large'} onPress={() => alert('Hello world!')}>warning</MDZButton>
+        <MDZButton color={'warning'} size={'small'} onPress={() => alert('Hello world!')}>warning</MDZButton>
+        <MDZButton color={'warning'} size={'medium'} onPress={() => alert('Hello world!')}>warning</MDZButton>
+        <MDZButton color={'warning'} size={'large'} onPress={() => alert('Hello world!')}>warning</MDZButton>
       </div>
       {/* // info */}<br />
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'info'} size={'small'} onPress={() => alert('Hello world!')}>info</MDZButton>
-        <MDZButton intent={'info'} size={'medium'} onPress={() => alert('Hello world!')}>info</MDZButton>
-        <MDZButton intent={'info'} size={'large'} onPress={() => alert('Hello world!')}>info</MDZButton>
+        <MDZButton color={'info'} size={'small'} onPress={() => alert('Hello world!')}>info</MDZButton>
+        <MDZButton color={'info'} size={'medium'} onPress={() => alert('Hello world!')}>info</MDZButton>
+        <MDZButton color={'info'} size={'large'} onPress={() => alert('Hello world!')}>info</MDZButton>
       </div>
       {/* // gray */}<br />
       <div className=" flex justify-center align-items-center">
-        <MDZButton intent={'gray'} size={'small'} onPress={() => alert('Hello world!')}>gray</MDZButton>
-        <MDZButton intent={'gray'} size={'medium'} onPress={() => alert('Hello world!')}>gray</MDZButton>
-        <MDZButton intent={'gray'} size={'large'} onPress={() => alert('Hello world!')}>gray</MDZButton>
+        <MDZButton color={'gray'} size={'small'} onPress={() => alert('Hello world!')}>gray</MDZButton>
+        <MDZButton color={'gray'} size={'medium'} onPress={() => alert('Hello world!')}>gray</MDZButton>
+        <MDZButton color={'gray'} size={'large'} onPress={() => alert('Hello world!')}>gray</MDZButton>
       </div>
     </div>
   )
--- mdz-ui/src/components/Button.tsx
+++ mdz-ui/src/components/Button.tsx
@@ -1,132 +1,37 @@
-import { useButton } from 'react-aria';
-import { useRef } from 'react';
-import { cva, type VariantProps } from "class-variance-authority";
+import { Button } from 'react-aria-components';
 
-const button = cva("button",{
-  variants: {
-    intent: {
-      primary: [
-        "bg-primary-9",
-        "text-primary-1",
-        "border-transparent",
-        "hover:bg-primary-10",
-        "hover:border-primary-5",
-        "focus:ring-primary-7",
-        "focus:outline-primary-7",
-      ],
-      secondary: [
-        "bg-secondary-9",
-        "text-secondary-1",
-        "border-transparent",
-        "hover:bg-secondary-10",
-        "hover:border-secondary-5",
-        "focus:ring-secondary-7",
-        "focus:outline-secondary-7",
-      ],
-      success: [
-        "bg-success-9",
-        "text-success-1",
-        "border-transparent",
-        "hover:bg-success-10",
-        "hover:border-success-5",
-        "focus:ring-success-7",
-        "focus:outline-success-7",
-      ],
-      error: [
-        "bg-error-9",
-        "text-error-1",
-        "border-transparent",
-        "hover:bg-error-10",
-        "hover:border-error-5",
-        "focus:ring-error-7",
-        "focus:outline-error-7",
-      ],
-      warning: [
-        "bg-warning-9",
-        "text-warning-1",
-        "border-transparent",
-        "hover:bg-warning-10",
-        "hover:border-warning-5",
-        "focus:ring-warning-7",
-        "focus:outline-warning-7",
-      ],
-      info: [
-        "bg-info-9",
-        "text-info-1",
-        "border-transparent",
-        "hover:bg-info-10",
-        "hover:border-info-5",
-        "focus:ring-info-7",
-        "focus:outline-info-7",
-        "dark:bg-primary-1"
-      ],
-      gray: [
-        "bg-gray-9",
-        "text-gray-1",
-        "border-transparent",
-        "hover:bg-gray-10",
-        "hover:border-gray-5",
-        "focus:ring-gray-7",
-        "focus:outline-gray-7",
-      ]
-    },
-    size: {
-      small: ["text-sm", "py-1", "px-2"],
-      medium: ["text-base", "py-2", "px-4"],
-      large: ["text-lg", "py-3", "px-6"],
-    },
-    appearence: {
-      solid: ["border-0"],
-      outline: ["border-2", "bg-transparent"]
-    }
-  },
-  compoundVariants: [
-    // Applied via:
-    //   `button({ intent: "primary", size: "medium" })`
-    //     or
-    //   `button({ intent: "secondary", size: "medium" })`
-    {
-      intent: "primary",
-      appearence: "solid",
-      size: "medium",
-      class:[
-        "bg-primary-9",
-        "text-primary-1",
-        "border-transparent",
-        "hover:bg-primary-10",
-        "hover:border-primary-5",
-        "focus:ring-primary-7",
-        "focus:outline-primary-7",
-      ],
-    },
-    {
-      intent: "primary",
-      appearence: "outline",
-      size: "medium",
-      class:[
-        "bg-error-9",
-        "text-error-1",
-        "border-transparent",
-        "hover:bg-error-10",
-        "hover:border-error-5",
-        "focus:ring-error-7",
-        "focus:outline-error-7",
-      ],
-    }
-  ],
-  defaultVariants: {
-    intent: "primary",
-    size: "medium",
-  },
-});
+interface ButtonProps {
+  onClick?: () => void;
+  color?: "primary" | "secondary" | "default";
+  variant?: "solid" | "outline";
+  children: React.ReactNode;
+}
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof button> { }
+const getStyleFromProps = (color: ButtonProps['color'], variant: ButtonProps['variant']) => {
 
-export const MDZButton: React.FC<ButtonProps> = (props) => {
-  const ref = useRef();
-  const { buttonProps } = useButton(props, ref);
-  const { className, intent, size, children } = props;
-  return <button className={button({ intent, size, className })}  {...buttonProps} ref={ref} > {children}</button>;
+  if (variant === 'solid') {
+    return `bg-${color}-9
+    text-${color}-1
+    border-transparent
+    hover:bg-${color}-10
+    hover:border-${color}-5
+    focus:ring-${color}-7
+    focus:outline-${color}-7`;
+  } else if (variant === 'outline') {
+    return `bg-primary-3 border-4 border-indigo-600`;
+  } else if (variant === 'link') {
+    return `bg-${color}-600`;
+
+  }
+}
+
+
+export const MDZButton: React.FC<ButtonProps> = ({ onClick, color = 'default', variant = 'solid', children }) => {
+  const buttonClasses = getStyleFromProps(color, variant);
+console.log(buttonClasses)
+  return (
+    <Button onPress={onClick} className={buttonClasses}>
+      {children}
+    </Button>
+  );
 };
